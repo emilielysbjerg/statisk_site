ffa40839f68da9847135f93705dd8201bab2f6ed
Emilie <Emilie>
2026-03-05 08:49:56 +0100

    7 commit

--- js/productlist.js
+++ js/productlist.js
@@ -1,21 +1,41 @@
+document
+  .querySelectorAll("button")
+  .forEach((knap) => knap.addEventListener("click", filter));
+
+let allData;
+
+function getData() {
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      allData = data;
+      showProducts(allData);
+    });
+}
+
+function filter(e) {
+  const valgt = e.target.textContent;
+  console.log(valgt);
+  if (valgt == "All") {
+    showProducts(allData);
+  } else {
+    const udsnit = allData.filter((element) => element.gender == valgt);
+    showProducts(udsnit);
+  }
+}
+
 const klikKategori = new URLSearchParams(window.location.search).get(
   "category",
 );
 
 const container = document.querySelector(".galleri");
 
-const endpoint = `https://kea-alt-del.dk/t7/api/products?category=${klikKategori}`;
+const endpoint = `https://kea-alt-del.dk/t7/api/products?category=${klikKategori}&limit=50`;
 
-function getData() {
-  fetch(endpoint)
-    .then((res) => res.json())
-    .then(showData);
-}
-
-function showData(json) {
+function showProducts(json) {
   let markup = "";
   json.forEach((product) => {
-    console.log(product);
+    //console.log(product);
 
     markup += `
           <article class="${product.soldout && "udsolgt"}">
